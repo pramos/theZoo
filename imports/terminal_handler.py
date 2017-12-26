63cf22a3254799c8aea7429fa2fed69f3a39918b
Pablo Ramos <pramos@users.noreply.github.com>
2017-12-26 10:39:53 +0000

    Flake 8 - terminal_handler.py: Fix nits. Review rlcompleter

--- imports/terminal_handler.py
+++ imports/terminal_handler.py
@@ -10,7 +10,7 @@ from imports import globals
 from imports import manysearches
 from imports.update_handler import Updater
 from imports import db_handler
-from imports.colors import *
+from imports.colors import red, green, blue, bold
 
 # Compatilibility to Python3
 if sys.version_info.major == 3:
@@ -27,7 +27,8 @@ class Controller:
         self.modules = None
         self.currentmodule = None
         self.db = db_handler.DBHandler()
-        self.commands = [("search", "Search for malwares according to a filter,\n\t\t\te.g 'search cpp worm'."),
+        self.commands = [("search", "Search for malwares according to a filter"
+                          + ",\n\t\t\te.g 'search cpp worm'."),
                          ("list all", "Lists all available modules"),
                          ("use", "Selects a malware by ID"),
                          ("info", "Retreives information about malware"),
@@ -37,10 +38,14 @@ class Controller:
                          ("help", "Displays this help..."),
                          ("exit", "Exits...")]
 
-        self.commandsWithoutDescription = {'search': '', 'list all': '', 'use': '', 'info': '',
-                                           'get': '', 'report-mal': '', 'update-db': '', 'help': '', 'exit': ''}
+        self.commandsWithoutDescription = {'search': '', 'list all': '',
+                                           'use': '', 'info': '',
+                                           'get': '', 'report-mal': '',
+                                           'update-db': '', 'help': '',
+                                           'exit': ''}
 
-        self.searchmeth = [("arch", "which architecture etc; x86, x64, arm7 so on..."),
+        self.searchmeth = [("arch",
+                            "which architecture etc; x86, x64, arm7 so on..."),
                            ("plat",
                             "platform: win32, win64, mac, android so on..."),
                            ("lang", "c, cpp, vbs, bin so on..."),
@@ -60,14 +65,19 @@ class Controller:
         while (True):  # Dont hate, affiliate
             try:
                 if self.currentmodule is not None:
-                    just_print = self.db.query("SELECT NAME FROM Malwares WHERE ID=?", self.currentmodule)[0][0]
+                    just_print = self.db.query(
+                        "SELECT NAME FROM Malwares WHERE ID=?",
+                        self.currentmodule)[0][0]
                     cmd = raw_input(
-                        bold(green('mdb ')) + bold(blue(just_print)) + green('#> ')).strip()
+                        bold(green('mdb ')) +
+                        bold(blue(just_print)) + green('#> ')).strip()
                 else:
                     cmd = raw_input(
                         bold(green('mdb ')) + green('#> ')).strip()
             except KeyboardInterrupt:
-                print(bold(blue("\n\n[*]")) + " Hope you enjoyed your visit at" + bold(red(" theZoo")) + "!")
+                print(bold(blue("\n\n[*] ")) +
+                      "Hope you enjoyed your visit at" +
+                      bold(red(" theZoo")) + "!")
                 exit()
 
             self.actOnCommand(cmd)
@@ -131,11 +141,15 @@ class Controller:
                 print("-------------- Begin of theZoo Report --------------")
                 print(report)
                 print("-------------- Ending of theZoo Report --------------")
-                print("To avoid compromising your privacy we have chose this method of reporting.")
-                print("If you have not stated your name we will not write a thanks in our README.")
-                print("Your email will remain private in scenario and will not be published.")
+                print("To avoid compromising your privacy we" +
+                      " have chose this method of reporting.")
+                print("If you have not stated your name we will" +
+                      " not write a thanks in our README.")
+                print("Your email will remain private in scenario" +
+                      " and will not be published.")
                 print("")
-                print("Please create an archive file with the structure described in the README file")
+                print("Please create an archive file with the structure" +
+                      " described in the README file")
                 print("And attach it to the email. ")
                 print("Please send this report to %s" % email)
 
@@ -163,16 +177,23 @@ class Controller:
             if cmd == 'list all':
                 print("\nAvailable Payloads:")
                 manySearch = manysearches.MuchSearch()
-                manySearch.print_payloads(self.db.get_mal_list(), ["%", "Name", "Type"])
+                manySearch.print_payloads(self.db.get_mal_list(),
+                                          ["%", "Name", "Type"])
                 return
 
             if cmd == 'info':
                 if self.currentmodule is None:
-                    print(red("[!] ") + "First select a malware using the \'use\' command")
+                    print(red("[!] ") +
+                          "First select a malware using the \'use\' command")
                     return
                 m = self.db.get_mal_info(self.currentmodule)
                 manySearch = manysearches.MuchSearch()
-                manySearch.print_payloads(m, ["%", "Name", "Ver.", "Author", "Lang", "Date", "Arch.", "Plat.", "Tags"])
+                manySearch.print_payloads(
+                    m,
+                    [
+                        "%", "Name", "Ver.", "Author", "Lang",
+                        "Date", "Arch.", "Plat.", "Tags"
+                    ])
                 return
 
             if cmd == 'quit':
